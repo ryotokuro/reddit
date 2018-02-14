0d48b7fab9b73679e72813dbf9fd413666be24a1
Taiyue Tan <ryotokuro@gmail.com>
2018-02-15 02:29:14 +1100

    reddit challenge unsolved (med level)

---
+++ challenge346.py
@@ -0,0 +1,13 @@
+string = input()
+# cut off left and right side?
+# interpret letters and assign numbers
+set from {0-9}
+
+# addition only
+(string a) + (string b)
+
+# store claim for validation
+claim = rightHS
+for numbers in leftHS
+    if eval numbers == claim
+        return dictionary
